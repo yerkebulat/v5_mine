e7a8b59903dc9cb9adcebf630fb035cdafe79dae
Yerkebulan <yerkebulantazabek@gmail.com>
2024-02-04 03:50:42 +0600

    0.01 percents dataset/higher precision

--- v_6/v_6_gaussian_process.py
+++ v_6/v_6_gaussian_process.py
@@ -1,28 +1,29 @@
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
 train_data = pd.read_excel("data_for_Test_and_Train.xlsx", sheet_name="Original")
-
 sampled_data = train_data.sample(frac=0.01, random_state=42)
-
 sampled_data['X'] = pd.to_numeric(sampled_data['X'], errors='coerce')
 sampled_data['Y'] = pd.to_numeric(sampled_data['Y'], errors='coerce')
 
 X_train = sampled_data[['X', 'Y']]
 y_train = sampled_data['Cu']
 
-dt_model = DecisionTreeRegressor(random_state=42)
-dt_model.fit(X_train, y_train)
+kernel = 1.0 * RBF()
+gp_model = GaussianProcessRegressor(kernel=kernel)
+gp_model.fit(X_train, y_train)
 
 x_vals = np.linspace(sampled_data['X'].min(), sampled_data['X'].max(), 1000)
 y_vals = np.linspace(sampled_data['Y'].min(), sampled_data['Y'].max(), 1000)
 x_mesh, y_mesh = np.meshgrid(x_vals, y_vals)
 
 points_to_predict = np.c_[x_mesh.ravel(), y_mesh.ravel()]
-predicted_values = dt_model.predict(points_to_predict)
+
+predicted_values, _ = gp_model.predict(points_to_predict, return_std=True)
 predicted_values_mesh = predicted_values.reshape(1000, 1000)
 cmap = sns.diverging_palette(240, 10, as_cmap=True)
 
@@ -33,7 +34,7 @@ heatmap = sns.heatmap(predicted_values_mesh,
                       vmax=1.0,
                       square=True,
                       cbar_kws={"label": "Cu Concentration"})
-plt.title("Copper Concentration Heatmap (Predicted values - Decision Tree Regression)")
+plt.title("Copper Concentration Heatmap (Predicted values - Gaussian Process Regression)")
 plt.xlabel("X")
 plt.ylabel("Y")
 
